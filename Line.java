e13ee982f7dda663ca84614f48473a738c8e368c
Victor-Aleix <79250111+Victor-Aleix@users.noreply.github.com>
2021-03-16 23:21:10 +0100

    Add files via upload

--- Line.java
+++ Line.java
@@ -7,52 +7,40 @@ public class Line {
     ArrayList<Character> text;
 
     public Line() {
-
         this.cursor = 0;
         this.text = new ArrayList<Character>();
-
     }
 
     public void right() {
         if (cursor < text.size()) {
             cursor++;
-            // System.out.print("\033[C");
         }
     }
 
     public void left() {
         if (cursor > 0) {
             cursor--;
-            // System.out.print("\033[D");
         }
     }
 
     public void home() {
-        // int aux = cursor;
         cursor = 0;
-        // System.out.print("\033[" + aux + "D");
     }
 
     public void end() {
-        // int aux = line.size() - cursor;
         cursor = text.size();
-        // System.out.print("\033[" + aux + "C");
     }
 
     public void delete() {
         if (cursor > 0) {
             cursor--;
             text.remove(cursor);
-            // System.out.print("\033[D");
-            // System.out.print("\033[P"); // redefine el comportamiento de la letra
         }
     }
 
     public void supr() {
         if (cursor < text.size()) {
             text.remove(cursor);
-            // System.out.print("\033[P");
         }
     }
-
 }
