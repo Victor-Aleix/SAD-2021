82aff4a3a4dac23ecf49df13bbf6b11869d6d5e5
Victor-Aleix <79250111+Victor-Aleix@users.noreply.github.com>
2021-04-06 16:09:05 +0200

    Add files via upload

--- hashcracker/main.py
+++ hashcracker/main.py
@@ -2,7 +2,7 @@ import hashlib
 import time
 import argparse
 from time import sleep
-
+import hashing
 
 # parsing arguments from terminal input
 parser = argparse.ArgumentParser()
@@ -10,58 +10,26 @@ parser.add_argument('-w', '--wordlist')  # wordlist
 parser.add_argument('-f', '--hashfile')  # hash
 parser.add_argument('-m', '--mode')  # hashing algorith,
 args = parser.parse_args()
+
+#reading wordlist and hash file
 with open(args.wordlist, errors='ignore') as f:
     content = f.read().splitlines()
 with open(args.hashfile) as f:
     hash = f.readline().splitlines()
 
-# defining functions for encoding strings
-
-
-def md5():
-    print("Trying to crack the hash " + hash[0] + "...")
-    for i in range(0, len(content)):
-        result = hashlib.md5(content[i].encode())
-        if(result.hexdigest().lower() == hash[0].lower()):
-            print("Hash cracked, the decoded string is : " + content[i])
-
-
-def sha1():
-    print("Trying to crack the hash " + hash[0] + "...")
-    for i in range(0, len(content)):
-        result = hashlib.sha1(content[i].encode())
-        if(result.hexdigest().lower() == hash[0].lower()):
-            print("Hash cracked, the decoded string is : " + content[i])
-
-
-def sha256():
-    print("Trying to crack the hash " + hash[0] + "...")
-    for i in range(0, len(content)):
-        result = hashlib.sha256(content[i].encode())
-        if(result.hexdigest().lower() == hash[0].lower()):
-            print("Hash cracked, the decoded string is : " + content[i])
-
-
-def nltm():
-    print("Trying to crack the hash " + hash[0] + "...")
-    for i in range(0, len(content)):
-        result = hashlib.new('md4', content[i].encode('utf-16le'))
-        if(result.hexdigest().lower() == hash[0].lower()):
-            print("Hash cracked, the decoded string is : " + content[i])     
-
-
 # current timestamp
 start = time.time()
 
+#crack hash
 print(args.mode)
 if(args.mode == 'sha1'):
-    sha1()
+    hashing.sha1(content,hash)
 elif(args.mode == 'md5'):
-    md5()
+    hashing.md5(content,hash)
 elif(args.mode == 'sha256'):
-    sha256()
+    hashing.sha256(content,hash)
 elif(args.mode == 'nltm'):
-    nltm()
+    hashing.nltm(content,hash)
 
 # timestamp when finished
 end = time.time()
