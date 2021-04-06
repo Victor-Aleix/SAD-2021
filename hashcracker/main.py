23c081edd3e6c4816344a4784c38f9cb31122555
Victor-Aleix <79250111+Victor-Aleix@users.noreply.github.com>
2021-04-06 15:55:19 +0200

    Add files via upload

--- hashcracker/main.py
+++ hashcracker/main.py
@@ -1,11 +1,8 @@
 import hashlib
-import threading
-import timeit
+import time
 import argparse
+from time import sleep
 
-
-# time starts counting
-start = timeit.timeit()
 
 # parsing arguments from terminal input
 parser = argparse.ArgumentParser()
@@ -13,7 +10,7 @@ parser.add_argument('-w', '--wordlist')  # wordlist
 parser.add_argument('-f', '--hashfile')  # hash
 parser.add_argument('-m', '--mode')  # hashing algorith,
 args = parser.parse_args()
-with open(args.wordlist) as f:
+with open(args.wordlist, errors='ignore') as f:
     content = f.read().splitlines()
 with open(args.hashfile) as f:
     hash = f.readline().splitlines()
@@ -48,19 +45,14 @@ def sha256():
 def nltm():
     print("Trying to crack the hash " + hash[0] + "...")
     for i in range(0, len(content)):
-        result = hashlib.new('md4',content[i].encode('utf-16le'))
+        result = hashlib.new('md4', content[i].encode('utf-16le'))
         if(result.hexdigest().lower() == hash[0].lower()):
-            print("Hash cracked, the decoded string is : " + content[i])
+            print("Hash cracked, the decoded string is : " + content[i])     
 
 
-# create and join threads
-""" def jointhreads():
-    for i in range(0, numberofthreads):
-        threads[i].join()
-        
-jointhreads() """
+# current timestamp
+start = time.time()
 
-# main thread
 print(args.mode)
 if(args.mode == 'sha1'):
     sha1()
@@ -70,5 +62,8 @@ elif(args.mode == 'sha256'):
     sha256()
 elif(args.mode == 'nltm'):
     nltm()
-end = timeit.timeit()
+
+# timestamp when finished
+end = time.time()
+
 print("Time elapsed : " + str(end - start) + " seconds")
