0079c71172e0786196bcbc168f169cfc8669d7bc
Victor-Aleix <79250111+Victor-Aleix@users.noreply.github.com>
2021-04-07 09:51:44 +0200

    Add files via upload

--- hashcracker/guihashing.py
+++ hashcracker/guihashing.py
@@ -1,37 +1,34 @@
 import hashlib
+import tkinter
+from tkinter import *
 
 # defining functions for encoding strings
 
-
-def md5(content, hash):
-    print("Trying to crack the hash " + hash[0] + "...")
+def guimd5(content, hash):
     for i in range(0, len(content)):
         result = hashlib.md5(content[i].encode())
         if(result.hexdigest().lower() == hash[0].lower()):
-            print("Hash cracked, the decoded string is : " + content[i])
+            return content[i]
             break
 
 
-def sha1(content, hash):
-    print("Trying to crack the hash " + hash[0] + "...")
+def guisha1(content, hash):
     for i in range(0, len(content)):
         result = hashlib.sha1(content[i].encode())
         if(result.hexdigest().lower() == hash[0].lower()):
-            print("Hash cracked, the decoded string is : " + content[i])
+            return content[i]
             break
 
-def sha256(content, hash):
-    print("Trying to crack the hash " + hash[0] + "...")
+def guisha256(content, hash):
     for i in range(0, len(content)):
         result = hashlib.sha256(content[i].encode())
         if(result.hexdigest().lower() == hash[0].lower()):
-            print("Hash cracked, the decoded string is : " + content[i])
+            return content[i]
             break
 
-def nltm(content, hash):
-    print("Trying to crack the hash " + hash[0] + "...")
+def guinltm(content, hash):
     for i in range(0, len(content)):
         result = hashlib.new('md4', content[i].encode('utf-16le'))
         if(result.hexdigest().lower() == hash[0].lower()):
-            print("Hash cracked, the decoded string is : " + content[i])
+            return content[i]
             break
--- hashcracker/main.py
+++ hashcracker/main.py
@@ -3,6 +3,7 @@ import time
 import argparse
 from time import sleep
 import hashing
+import bruteforce
 
 # parsing arguments from terminal input
 parser = argparse.ArgumentParser()
@@ -11,7 +12,7 @@ parser.add_argument('-f', '--hashfile')  # hash
 parser.add_argument('-m', '--mode')  # hashing algorith,
 args = parser.parse_args()
 
-#reading wordlist and hash file
+# reading wordlist and hash file
 with open(args.wordlist, errors='ignore') as f:
     content = f.read().splitlines()
 with open(args.hashfile) as f:
@@ -20,16 +21,16 @@ with open(args.hashfile) as f:
 # current timestamp
 start = time.time()
 
-#crack hash
+# crack hash
 print(args.mode)
 if(args.mode == 'sha1'):
-    hashing.sha1(content,hash)
+    hashing.sha1(content, hash)
 elif(args.mode == 'md5'):
-    hashing.md5(content,hash)
+    hashing.md5(content, hash)
 elif(args.mode == 'sha256'):
-    hashing.sha256(content,hash)
+    hashing.sha256(content, hash)
 elif(args.mode == 'nltm'):
-    hashing.nltm(content,hash)
+    hashing.nltm(content, hash)
 
 # timestamp when finished
 end = time.time()
