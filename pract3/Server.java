cc73e0a4569f3562714d15fdb0a01e2f3d9823d2
Victor-Aleix <79250111+Victor-Aleix@users.noreply.github.com>
2021-06-15 21:24:57 +0200

    Update Server.java

--- pract3/Server.java
+++ pract3/Server.java
@@ -1,21 +1,18 @@
 package src.server;
 
 import java.io.IOException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+
 
 public class Server {
     private static final int PORT = 1234;
-    private static DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
 
     public static void main(String[] args) {
         try {
             Reactor r  = new Reactor(PORT);
             new Thread(r).start();
-            System.out.println(dateFormat.format(new Date())+" === Server === Waiting for a connection...");
+            System.out.println(" === Server === Waiting for a connection...");
         } catch (IOException e) {
-            System.out.println(dateFormat.format(new Date())+" === Server === Error");
+            System.out.println(" === Server === Error");
         }
     }
 }
